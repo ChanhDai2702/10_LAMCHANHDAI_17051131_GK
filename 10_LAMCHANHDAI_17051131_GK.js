917604045cc63ef61fb4afcec7c6d90b9cbf14c6
ChanhDai2702 <daiviet1023@gmail.com>
2020-11-08 09:46:35 +0700

    first commit

---
+++ 10_LAMCHANHDAI_17051131_GK.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+var AWS = require('aws-sdk');
+var multer = require('multer');
+var storage = multer.diskStorage({
+    destination : function (req,file,cb){
+        cb(null,'/uploads');
+    },
+    filename: function (req,file,cb){
+        cb(null,file.originalname);
+    }
+})
+ 
+var upload = multer({storage:storage})
+app.set("view engine","ejs");
+app.set("views","./views");
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+AWS.config.update({
+    region: "ap-southeast-1",
+    endpoint: "http://dynamodb.ap-southeast-1.amazonaws.com"
+});
+var dynamodb = new AWS.DynamoDB();
+var docClient = new AWS.DynamoDB.DocumentClient();
+ 
+
+function findUser (res) {
+    let params = {
+        TableName: "SanPham"
+    };
+    docClient.scan(params, function (err, data) {
+        if (err) {
+            console.log(JSON.stringify(err, null, 2));
+        } else {
+            if(data.Items.length === 0){
+                res.end(JSON.stringify({message :'Table rỗng '}));
+            }
+            res.render('DanhSachSP.ejs',{
+                data : data.Items
+            });
+        }
+    });
+ 
+}
+app.get("/",(req,res)=>{
+    findUser(res);
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+})
